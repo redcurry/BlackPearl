3bbff5181c4153e1f9333a2694c003a613125bbc
Carlos Anderson <carlosjanderson@gmail.com>
2019-02-17 18:37:36 -0500

    Refactor to put circle tests in a separate file

---
+++ exp/TestOpenCv/TestOpenCv.Coin/HoughCircleTest.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void onCanny1Change(int pos, void* param);
+void onCanny2Change(int pos, void* param);
+void onAccumChange(int pos, void* param);
+void detect_and_draw_circles();
+int start(int argc, char** argv);
