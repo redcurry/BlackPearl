2970882127773833fc2b338e55b2a3dca0fa8cc4
Carlos Anderson <carlosjanderson@gmail.com>
2019-02-17 01:32:56 -0500

    Add shared interface to ImageAnalysis library

---
+++ src/BlackPearl.Xamarin/BlackPearl.Xamarin/IImageAnalysis.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackPearl.Xamarin
+{
+    public interface IImageAnalysis
+    {
+        double GetMean();
+    }
+}
