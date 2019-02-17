4a2e01e79120cfebb28ee9cf5890e9625775fe60
Carlos Anderson <carlosjanderson@gmail.com>
2019-02-17 01:45:45 -0500

    Add ImageAnalysis implementation in iOS project

---
+++ src/BlackPearl.Xamarin/BlackPearl.Xamarin.iOS/ImageAnalysis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BlackPearl.Xamarin.iOS.ImageAnalysis))]
+namespace BlackPearl.Xamarin.iOS
+{
+    public class ImageAnalysis : IImageAnalysis
+    {
+        public double GetMean()
+        {
+            return new global::ImageAnalysis.Bindings.ImageAnalysis().GetMean();
+        }
+    }
+}
