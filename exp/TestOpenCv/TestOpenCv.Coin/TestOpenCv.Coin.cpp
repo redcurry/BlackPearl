04e6598207d79615bcbc4de2ad2c4d3bbdd6ddf0
Carlos Anderson <carlosjanderson@gmail.com>
2019-02-17 13:48:37 -0500

    Write basic code to show OpenCV works

--- exp/TestOpenCv/TestOpenCv.Coin/TestOpenCv.Coin.cpp
+++ exp/TestOpenCv/TestOpenCv.Coin/TestOpenCv.Coin.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n"; 
+	cv::Mat img = cv::imread(argv[1], -1);
+	if (img.empty()) return -1;
+	cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Example1", img);
+	cv::waitKey(0);
+	cv::destroyWindow("Example1");
+	return 0;
 }
