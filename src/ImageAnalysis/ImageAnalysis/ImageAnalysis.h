c8fae293e9abfe01a3729b5015c1c6c1ed6d6e67
Carlos Anderson <carlosjanderson@gmail.com>
2019-02-16 23:25:14 -0500

    Add blank Cocoa Touch Static Library project

---
+++ src/ImageAnalysis/ImageAnalysis/ImageAnalysis.h
@@ -0,0 +1,13 @@
+//
+//  ImageAnalysis.h
+//  ImageAnalysis
+//
+//  Created by Carlos Anderson on 2/16/19.
+//  Copyright © 2019 Carlos Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageAnalysis : NSObject
+
+@end
---
+++ src/ImageAnalysis/ImageAnalysis/ImageAnalysis.m
@@ -0,0 +1,13 @@
+//
+//  ImageAnalysis.m
+//  ImageAnalysis
+//
+//  Created by Carlos Anderson on 2/16/19.
+//  Copyright © 2019 Carlos Anderson. All rights reserved.
+//
+
+#import "ImageAnalysis.h"
+
+@implementation ImageAnalysis
+
+@end
